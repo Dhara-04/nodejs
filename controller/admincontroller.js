b844bbb238495afbea8adaff2203289af05a9625
Dhara-04 <dharasavaliya06@gmail.com>
2025-02-04 14:34:36 +0530

    dhara-04

---
+++ controller/admincontroller.js
@@ -0,0 +1,59 @@
+var user = require("../model/usermodel");
+
+exports.insert = async(req,res)=> {
+    user.create(req.body);
+    res.status(200).json({
+        status: "success"
+    })
+}
+
+exports.get_data = async(req,res)=> {
+    var limit = 3;
+    var page_no = req.query.page_no;
+    if(page_no == undefined){
+        page_no=1;
+    }
+    var start = (page_no-1)*limit;
+    var total_rec = await user.find().countDocuments();
+    var total_page = Math.ceil(total_rec/limit);
+
+    var data = await user.find().skip(start).limit(limit);
+    // var data = await user.find().countDocuments();
+    // var data = await user.find().skip(1);
+    // var data = await user.find().limit(3);
+    // var data = await user.find().select("email").select("password");
+
+    res.status(200).json({
+        status: "success",
+        data,
+        page_no,
+        total_page
+    })
+}
+
+exports.get_update_data = async(req,res)=> {
+    var id = req.params.id;
+    var data = await user.findById(id)    
+    res.status(200).json({
+        status: "success",
+        data
+    })
+}
+
+exports.update_data = async(req,res)=> {
+    var id = req.params.id;
+    var data = await user.findByIdAndUpdate(id,req.body)    
+    res.status(200).json({
+        status: "success",
+        data
+    })
+}
+
+exports.delete_data = async(req,res)=> {
+    var id = req.params.id;
+    var data = await user.findByIdAndDelete(id)    
+    res.status(200).json({
+        status: "success",
+        data
+    })
+}
